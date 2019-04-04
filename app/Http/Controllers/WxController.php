98b92a30942fb1c397c0f5291f1b57dfe2c79eb1
zhang18735113793 <1139125486@qq.com>
2019-04-04 13:44:39 +0800

    123

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -8,9 +8,35 @@ class WxController extends Controller
 {
     public function index()
     {
-        $echostr=$_GET['echostr'];
-        if($this->signature()){
-            echo $echostr;exit;
+        $this->responseMsg();
+//        $echostr=$_GET['echostr'];
+//        if($this->signature()){
+//            echo $echostr;exit;
+//        }
+    }
+
+    public function responseMsg()
+    {
+        $poststr=file_get_contents("php://input");
+        $postobj=simplexml_load_string($poststr,SimpleXMLElement,lIBXML_NOCDATA);
+        $str="<xml>
+                  <ToUserName><![CDATA[toUser]]></ToUserName>
+                  <FromUserName><![CDATA[FromUser]]></FromUserName>
+                  <CreateTime>123456789</CreateTime>
+                  <MsgType><![CDATA[event]]></MsgType>
+                  <Event><![CDATA[subscribe]]></Event>
+                  <Content><![CDATA[%s]]></Content>
+               </xml>";
+        $toUserName=$postobj->ToUserName;
+        $fromUserName=$postobj->FromUserName;
+        $time=time();
+        $msgtype="text";
+        if($postobj->MsgType=='event'){
+            if($postobj->Event=='subscribe'){
+                $content="欢迎来到我的订阅号";
+                $res=sprintf($str,$toUserName,$fromUserName,$time,$msgtype,$content);
+                echo $res;exit;
+            }
         }
     }
     private function signature()
